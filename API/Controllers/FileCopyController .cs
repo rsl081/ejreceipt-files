60c813b7a023b880784572cf944b316ae7d403f4
John Russel Rodriguez <53944109+rsl081@users.noreply.github.com>
2024-08-16 16:44:51 +0800

    second init

--- API/Controllers/FileCopyController .cs
+++ API/Controllers/FileCopyController .cs
@@ -14,13 +14,14 @@ namespace API.Controllers
         private readonly string _destinationDirectory = @"C:\Users\jrrodriguez\Downloads\CEPA";
 
         [HttpPost("CopyFile")]
-        public IActionResult CopyFile([FromQuery] string fileName)
+        public IActionResult CopyFile([FromQuery] string fileName, string foldername)
         {
+            string _sourceDirectory = @"\\dev-apps-new\Files";
             try
             {
                 // Use Path.Combine to construct full file paths
                 string sourceFilePath = Path.Combine(_sourceDirectory, fileName + ".pdf");
-                string destinationFilePath = Path.Combine(_destinationDirectory, fileName + ".pdf");
+                string destinationFilePath = Path.Combine(_destinationDirectory, fileName + "_" + DateTime.Now.ToString("YYYYmmdd") + ".pdf");
 
                 // Check if the file exists
                 if (!System.IO.File.Exists(sourceFilePath))
